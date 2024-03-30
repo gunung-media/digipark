c10156f622e4c0374ced33a80f4824fdcc7258ac
Richie-Z <zakariarichie@yahoo.com>
2024-03-30 12:08:46 +0700

    feat: add job apply widget

--- app/Filament/Company/Widgets/JobApply.php
+++ app/Filament/Company/Widgets/JobApply.php
@@ -3,23 +3,37 @@
 namespace App\Filament\Company\Widgets;
 
 use App\Models\JobApplicant;
+use Carbon\Carbon;
 use Filament\Widgets\ChartWidget;
 use Flowframe\Trend\Trend;
 use Flowframe\Trend\TrendValue;
 
 class JobApply extends ChartWidget
 {
-    protected static ?string $heading = 'Chart';
+    protected static ?string $heading = 'Jumlah Pengapply';
+
+    protected int | string | array $columnSpan = 2;
 
     protected function getData(): array
     {
         $data = Trend::query(
-            JobApplicant::whereHas('job', function (Builder $query) {
+            JobApplicant::whereHas('job', function ($query) {
                 $query->where('company_id', auth('company')->user()->id);
             })
-        );
+        )->between(
+            start: now()->startOfMonth(),
+            end: now()->endOfMonth()
+        )
+            ->perDay()
+            ->count();
         return [
-            //
+            'datasets' => [
+                [
+                    'label' => 'Jumlah Pengapply',
+                    'data' => $data->map(fn (TrendValue $value) => $value->aggregate),
+                ],
+            ],
+            'labels' => $data->map(fn (TrendValue $value) => Carbon::parse($value->date)->format('d M, Y')),
         ];
     }
 
