39d45b6989c89e13f08792b50ce62111a6f0aecc
Richie-Z <zakariarichie@yahoo.com>
2024-04-03 13:42:35 +0700

    style(admin): dashboard form layouting

--- app/Filament/Admin/Resources/DashboardResource.php
+++ app/Filament/Admin/Resources/DashboardResource.php
@@ -14,50 +14,59 @@ use Filament\Tables\Table;
 class DashboardResource extends Resource
 {
     protected static ?string $model = Dashboard::class;
-
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
-
     protected static ?string $navigationGroup = 'Settings';
-
     protected static bool $shouldRegisterNavigation = false;
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                Forms\Components\RichEditor::make('short_description')
-                    ->columnSpanFull()
-                    ->maxLength(250)
-                    ->required(),
-                Forms\Components\RichEditor::make('mission')
-                    ->columnSpanFull()
-                    ->maxLength(150)
-                    ->required(),
-                Forms\Components\RichEditor::make('quote')
-                    ->default('"The purpose of life is not to be happy. It is to be useful, to be honorable, to be compassionate, to have it make some difference that you have lived and lived well."')
-                    ->columnSpanFull(),
-                Forms\Components\FileUpload::make('our_story_image')
-                    ->disk('public')
-                    ->directory('dashboard')
-                    ->image()
-                    ->columnSpanFull()
-                    ->required(),
-                Forms\Components\TextInput::make('facebook')
-                    ->prefixIcon('icon-fb'),
-                Forms\Components\TextInput::make('instagram')
-                    ->prefixIcon('icon-ig'),
-                Forms\Components\TextInput::make('twitter')
-                    ->prefixIcon('icon-x'),
-                Forms\Components\TextInput::make('youtube')
-                    ->prefixIcon('icon-yt'),
-                Forms\Components\TextInput::make('linkedin')
-                    ->prefixIcon('icon-linkedin'),
-                Forms\Components\TextInput::make('address'),
-                Forms\Components\TextInput::make('phone_number')
-                    ->prefixIcon('icon-wa'),
-                Forms\Components\TextInput::make('default_text')
-                    ->label('Default Text WA')
-                    ->default('Halo admin digipark'),
+                Forms\Components\Split::make([
+                    Forms\Components\Tabs::make()
+                        ->tabs([
+                            Forms\Components\Tabs\Tab::make('Our Story Section')->schema([
+                                Forms\Components\RichEditor::make('short_description')
+                                    ->columnSpanFull()
+                                    ->maxLength(250)
+                                    ->required(),
+                                Forms\Components\RichEditor::make('mission')
+                                    ->columnSpanFull()
+                                    ->maxLength(150)
+                                    ->required(),
+                                Forms\Components\FileUpload::make('our_story_image')
+                                    ->disk('public')
+                                    ->directory('dashboard')
+                                    ->image()
+                                    ->columnSpanFull()
+                                    ->required(),
+                            ]),
+                            Forms\Components\Tabs\Tab::make('Additional')
+                                ->schema([
+                                    Forms\Components\RichEditor::make('quote')
+                                        ->default('"The purpose of life is not to be happy. It is to be useful, to be honorable, to be compassionate, to have it make some difference that you have lived and lived well."')
+                                        ->columnSpanFull(),
+                                ])
+                        ]),
+                    Forms\Components\Section::make('Contact')->schema([
+                        Forms\Components\TextInput::make('facebook')
+                            ->prefixIcon('icon-fb'),
+                        Forms\Components\TextInput::make('instagram')
+                            ->prefixIcon('icon-ig'),
+                        Forms\Components\TextInput::make('twitter')
+                            ->prefixIcon('icon-x'),
+                        Forms\Components\TextInput::make('youtube')
+                            ->prefixIcon('icon-yt'),
+                        Forms\Components\TextInput::make('linkedin')
+                            ->prefixIcon('icon-linkedin'),
+                        Forms\Components\TextInput::make('address'),
+                        Forms\Components\TextInput::make('phone_number')
+                            ->prefixIcon('icon-wa'),
+                        Forms\Components\TextInput::make('default_text')
+                            ->label('Default Text WA')
+                            ->default('Halo admin digipark'),
+                    ])->grow(false)
+                ])->columnSpanFull(),
             ]);
     }
 
