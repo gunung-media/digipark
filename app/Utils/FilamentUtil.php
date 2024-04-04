5c2b8d7159035576f1c98b9bd24175ec1cef4183
Richie-Z <zakariarichie@yahoo.com>
2024-04-04 17:09:47 +0700

    feat(utils): introduce sendNotifToAdmin

--- app/Utils/FilamentUtil.php
+++ app/Utils/FilamentUtil.php
@@ -2,9 +2,12 @@
 
 namespace App\Utils;
 
+use App\Models\User;
 use Exception;
+use Filament\Actions\Action;
 use Filament\Facades\Filament;
 
+use Filament\Notifications\Notification;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Contracts\Auth\Authenticatable;
 
@@ -17,5 +20,17 @@ class FilamentUtil
             throw new Exception('The authenticated user object must be an Eloquent model to allow the profile page to update it.');
         }
         return $user;
+    }
+
+    public static function sendNotifToAdmin(string $url, string $title, ?string $body): void
+    {
+        Notification::make()
+            ->title($title)
+            ->body($body)
+            ->info()
+            ->actions([
+                Action::make('View')->url($url)->button(),
+            ])
+            ->sendToDatabase(User::all());
     }
 }
