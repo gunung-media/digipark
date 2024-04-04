b2793ad2775781a610b3128f61ec65ccb2f1e428
Richie-Z <zakariarichie@yahoo.com>
2024-04-04 10:29:55 +0700

    feat(seeker): add redirector if the additional profile is null

--- app/Filament/Seeker/Resources/ClaimJhtResource/Pages/CreateClaimJht.php
+++ app/Filament/Seeker/Resources/ClaimJhtResource/Pages/CreateClaimJht.php
@@ -2,11 +2,26 @@
 
 namespace App\Filament\Seeker\Resources\ClaimJhtResource\Pages;
 
+use App\Filament\Seeker\Pages\EditProfile;
 use App\Filament\Seeker\Resources\ClaimJhtResource;
+use App\Utils\FilamentUtil;
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateClaimJht extends CreateRecord
 {
     protected static string $resource = ClaimJhtResource::class;
+
+    public function mount(): void
+    {
+        if (is_null(FilamentUtil::getUser()->additional)) {
+            Notification::make()
+                ->warning()
+                ->title('Pengaturan profil anda belum lengkap. Silahkan lengkapi profil anda terlebih dahulu.')
+                ->send();
+            $this->redirect(EditProfile::getUrl());
+        }
+        parent::mount();
+    }
 }
