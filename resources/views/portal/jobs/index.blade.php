1b8c72eab1fed2295ac9bc34d36816a49110a1a9
Richie-Z <zakariarichie@yahoo.com>
2024-04-15 13:34:20 +0700

    style: responsive search bar

--- resources/views/portal/jobs/index.blade.php
+++ resources/views/portal/jobs/index.blade.php
@@ -21,14 +21,18 @@
                 <div class="row">
                     <div class="col-12" style="margin-bottom:0rem; margin-top: -2rem;">
                         <form class="job-form custom-form" method="get">
-                            <div class="col-lg-5 col-12">
-                                <div class="form-group">
-                                    <input type="" class="form-control" name="q" value="{{ $name }}"
-                                        placeholder="Nama Pekerjaan/Perusahaan">
+                            <div class="row" style="width:100%; justify-content:center">
+                                <div class="col-md-6 col-12">
+                                    <div class="form-group">
+                                        <input type="" class="form-control" name="q"
+                                            value="{{ $name }}" placeholder="Nama Pekerjaan/Perusahaan">
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="form-group">
-                                <button class="btn btn-primary custom-btn">Cari</button>
+                                <div class="col-md-1 col-12">
+                                    <div class="form-group">
+                                        <button class="btn btn-primary custom-btn">Cari</button>
+                                    </div>
+                                </div>
                             </div>
                         </form>
                     </div>
--- resources/views/portal/train-and-internship/index.blade.php
+++ resources/views/portal/train-and-internship/index.blade.php
@@ -25,14 +25,18 @@
                 <div class="row">
                     <div class="col-12" style="margin-bottom:0rem; margin-top: -2rem;">
                         <form class="job-form custom-form" method="get">
-                            <div class="col-lg-5 col-12">
-                                <div class="form-group">
-                                    <input type="" class="form-control" name="q" value="{{ $name }}"
-                                        placeholder="Nama Pelatihan/Magang">
+                            <div class="row" style="width:100%; justify-content:center">
+                                <div class="col-md-6 col-12">
+                                    <div class="form-group">
+                                        <input type="" class="form-control" name="q"
+                                            value="{{ $name }}" placeholder="Nama Pelatihan/Magang">
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="form-group">
-                                <button class="btn btn-primary custom-btn">Cari</button>
+                                <div class="col-md-1 col-12">
+                                    <div class="form-group">
+                                        <button class="btn btn-primary custom-btn">Cari</button>
+                                    </div>
+                                </div>
                             </div>
                         </form>
                     </div>
