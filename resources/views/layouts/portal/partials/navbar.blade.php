12e97df0403e80760d59788e0cb4a782d0ae153a
Richie-Z <zakariarichie@yahoo.com>
2024-04-15 21:58:33 +0700

    feat: navbar item

--- resources/views/layouts/portal/partials/navbar.blade.php
+++ resources/views/layouts/portal/partials/navbar.blade.php
@@ -15,7 +15,7 @@
           <div class="collapse navbar-collapse" id="navbarNav">
               <ul class="navbar-nav ms-auto">
                   <li class="nav-item">
-                      <a class="nav-link click-scroll" href="{{ route('landing') }}">Portal Layanan</a>
+                      <a class="nav-link click-scroll" href="{{ route('landing') }}">Portal</a>
                   </li>
                   <li class="nav-item">
                       <a class="nav-link click-scroll" href="{{ route('portal') }}">Beranda</a>
@@ -27,24 +27,57 @@
                       <ul class="dropdown-menu dropdown-menu-light" aria-labelledby="navbarLightDropdownMenuLink">
                           <li><a class="dropdown-item" href="/website#unit-layanan">Visi</a></li>
                           <li><a class="dropdown-item" href="/website#profil">Profil</a></li>
-                          <li><a class="dropdown-item" href="/website#pekerjaan">Pekerjaan</a></li>
-                          <li><a class="dropdown-item" href="/website#news">Berita</a></li>
+                          <!-- <li><a class="dropdown-item" href="/website#pekerjaan">Pekerjaan</a></li> -->
+                          <!-- <li><a class="dropdown-item" href="/website#news">Berita</a></li> -->
                       </ul>
                   </li>
+                  <!-- <li class="nav-item"> -->
+                  <!--     <a class="nav-link click-scroll" href="{{ route('portal.news.index') }}">Berita</a> -->
+                  <!-- </li> -->
                   <li class="nav-item">
-                      <a class="nav-link click-scroll" href="{{ route('portal.news.index') }}">Berita</a>
+                      <a class="nav-link click-scroll" href="{{ route('portal.jobs.index') }}">Pekerjaan</a>
                   </li>
-                  <li class="nav-item"><a class="nav-link click-scroll"
-                          href="{{ route('portal.jobs.index') }}">Pekerjaan</a></li>
                   <li class="nav-item dropdown">
                       <a class="nav-link click-scroll dropdown-toggle" href="#layanan" id="navbarLightDropdownMenuLink"
-                          role="button" data-bs-toggle="dropdown" aria-expanded="false">Layanan Masyarakat</a>
+                          role="button" data-bs-toggle="dropdown" aria-expanded="false">Layanan</a>
 
                       <ul class="dropdown-menu dropdown-menu-light" aria-labelledby="navbarLightDropdownMenuLink">
-                          <li><a class="dropdown-item" href="{{ route('portal.consultation') }}">Konsultasi</a></li>
+                          <li><a class="dropdown-item" href="{{ route('portal.consultation') }}">Layanan Konsultasi</a>
+                          </li>
                           <li>
                               <a class="dropdown-item" href="{{ route('portal.train-and-internship.index') }}">
                                   Informasi Pelatihan dan Magang
+                              </a>
+                          </li>
+                          <li><a class="dropdown-item"
+                                  href="{{ route('filament.seeker.resources.claim-jhts.create') }}">Klaim JHT</a>
+                          </li>
+                          <li><a class="dropdown-item"
+                                  href="{{ route('filament.company.resources.company-legalizations.create') }}">Pengesahan
+                                  Peraturan Perusahaan</a>
+                          </li>
+                          <li><a class="dropdown-item"
+                                  href="{{ route('filament.company.resources.company-laid-offs.create') }}">Laporan
+                                  PHK</a>
+                          </li>
+                          <li><a class="dropdown-item"
+                                  href="{{ route('filament.company.resources.jobs.create') }}">Pelaporan
+                                  Lowongan</a>
+                          </li>
+                          <li><a class="dropdown-item"
+                                  href="{{ route('filament.company.resources.placements.create') }}">Pelaporan
+                                  Penempatan</a>
+                          </li>
+                          <li><a class="dropdown-item"
+                                  href="{{ route('filament.company.resources.labor-demands.create') }}">Permintaan
+                                  Tenaga Kerja</a>
+                          </li>
+                          <li><a class="dropdown-item" href="{{ route('filament.admin.info-employment') }}">Info Data
+                                  Ketenagakerjaan</a>
+                          </li>
+                          <li>
+                              <a class="dropdown-item" href="https://account.kemnaker.go.id/register" target="_blank">
+                                  Pembuatan AK 1
                               </a>
                           </li>
                           <!-- <li><a class="dropdown-item" href="{{ route('portal.dummies.magang') }}">Informasi -->
