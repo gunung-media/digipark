d567599702008c108ffb9fb63363d865d89125dd
Richie-Z <zakariarichie@yahoo.com>
2024-05-03 12:51:46 +0700

    feat: add meta

--- resources/views/layouts/portal/app.blade.php
+++ resources/views/layouts/portal/app.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <title>@yield('title') Digital Palangka Raya Kreatif Kewirausahaan</title>
     @include('layouts.portal.partials.css')
     @yield('css')
