0a5ac9c64ea5edd0b5f12af2d16978012b40812a
Richie-Z <zakariarichie@yahoo.com>
2024-05-04 16:18:37 +0700

    feat: improve seo

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,12 +4,31 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description"
+        content="Digipark adalah situs dari Dinas Ketenagakerjaan Palangkaraya yang bertujuan untuk membantu masyarakat dan perusahaan dalam mencari dan menawarkan lowongan pekerjaan.">
+    <meta name="keywords"
+        content="digipark, dinas ketenagakerjaan, palangkaraya, lowongan kerja, mencari kerja, pekerjaan, karir">
+    <meta name="author" content="gunungmedia.com">
+    <meta property="og:title" content="Digipark - Situs Dinas Ketenagakerjaan Palangkaraya">
+    <meta property="og:description"
+        content="Digipark adalah situs dari Dinas Ketenagakerjaan Palangkaraya yang bertujuan untuk membantu masyarakat dan perusahaan dalam mencari dan menawarkan lowongan pekerjaan.">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://digipark.palangkaraya.go.id">
+    <meta property="og:image"
+        content="https://digipark.palangkaraya.go.id/storage/dashboard_images/01HTFN04GVF2N2KCRNVEPXYFP9.png">
+    <meta property="og:image:width" content="1200">
+    <meta property="og:image:height" content="630">
+    <meta property="og:locale" content="id_ID">
+    <meta property="og:site_name" content="Digipark">
 
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,100..900;1,100..900&display=swap"
         rel="stylesheet">
+    <link rel="icon"
+        href="https://palangkaraya.go.id/wp-content/uploads/2015/10/1bb4e13af45cc5c480069da8670508c5.ico_.png"
+        type="image/png">
 
     <style>
         * {
--- resources/views/layouts/portal/app.blade.php
+++ resources/views/layouts/portal/app.blade.php
@@ -4,7 +4,27 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description"
+        content="Digipark adalah situs dari Dinas Ketenagakerjaan Palangkaraya yang bertujuan untuk membantu masyarakat dan perusahaan dalam mencari dan menawarkan lowongan pekerjaan.">
+    <meta name="keywords"
+        content="digipark, dinas ketenagakerjaan, palangkaraya, lowongan kerja, mencari kerja, pekerjaan, karir">
+    <meta name="author" content="gunungmedia.com">
+    <meta property="og:title" content="Digipark - Situs Dinas Ketenagakerjaan Palangkaraya">
+    <meta property="og:description"
+        content="Digipark adalah situs dari Dinas Ketenagakerjaan Palangkaraya yang bertujuan untuk membantu masyarakat dan perusahaan dalam mencari dan menawarkan lowongan pekerjaan.">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://digipark.palangkaraya.go.id">
+    <meta property="og:image"
+        content="https://digipark.palangkaraya.go.id/storage/dashboard_images/01HTFN04GVF2N2KCRNVEPXYFP9.png">
+    <meta property="og:image:width" content="1200">
+    <meta property="og:image:height" content="630">
+    <meta property="og:locale" content="id_ID">
+    <meta property="og:site_name" content="Digipark">
+
     <title>@yield('title') Digital Palangka Raya Kreatif Kewirausahaan</title>
+    <link rel="icon"
+        href="https://palangkaraya.go.id/wp-content/uploads/2015/10/1bb4e13af45cc5c480069da8670508c5.ico_.png"
+        type="image/png">
     @include('layouts.portal.partials.css')
     @yield('css')
 </head>
