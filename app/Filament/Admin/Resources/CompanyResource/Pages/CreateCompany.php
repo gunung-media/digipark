d13ce31b527d6121d3b24ff19f472484b215166c
Richie-Z <zakariarichie@yahoo.com>
2024-05-07 00:05:07 +0700

    fix(resource): add handle record add password

--- app/Filament/Admin/Resources/CompanyResource/Pages/CreateCompany.php
+++ app/Filament/Admin/Resources/CompanyResource/Pages/CreateCompany.php
@@ -4,8 +4,14 @@ namespace App\Filament\Admin\Resources\CompanyResource\Pages;
 
 use App\Filament\Admin\Resources\CompanyResource;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Database\Eloquent\Model;
 
 class CreateCompany extends CreateRecord
 {
     protected static string $resource = CompanyResource::class;
+
+    protected function handleRecordCreation(array $data): Model
+    {
+        return parent::handleRecordCreation([...$data, 'password' => bcrypt('password')]);
+    }
 }
--- app/Filament/Admin/Resources/SeekerResource/Pages/CreateSeeker.php
+++ app/Filament/Admin/Resources/SeekerResource/Pages/CreateSeeker.php
@@ -4,8 +4,14 @@ namespace App\Filament\Admin\Resources\SeekerResource\Pages;
 
 use App\Filament\Admin\Resources\SeekerResource;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Database\Eloquent\Model;
 
 class CreateSeeker extends CreateRecord
 {
     protected static string $resource = SeekerResource::class;
+
+    protected function handleRecordCreation(array $data): Model
+    {
+        return parent::handleRecordCreation([...$data, 'password' => bcrypt('password')]);
+    }
 }
