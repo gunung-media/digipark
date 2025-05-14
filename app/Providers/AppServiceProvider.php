20658f4393ff2939f29e6442faf89aaee337e1d0
Richie-Z <zakariarichie@yahoo.com>
2025-05-14 11:48:59 +0200

    fix!: vsl

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use App\Models\Admin\Menu\Menu;
 use App\Models\Admin\Settings\Dashboard;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -24,14 +25,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        URL::forceScheme('https');
         Model::unguard();
         if (Schema::hasTable('dashboard') || Schema::hasTable('menus')) {
             View::share('dashboard', Dashboard::with([
-                'images' => fn ($query) => $query->active(),
-                'visions' => fn ($query) => $query->active(),
-                'testimonials' => fn ($query) => $query->active()
+                'images' => fn($query) => $query->active(),
+                'visions' => fn($query) => $query->active(),
+                'testimonials' => fn($query) => $query->active()
             ])->first());
-            View::share('menus', Menu::active()->with(['subMenus' => fn ($query) => $query->active()])->get());
+            View::share('menus', Menu::active()->with(['subMenus' => fn($query) => $query->active()])->get());
         }
     }
 }
